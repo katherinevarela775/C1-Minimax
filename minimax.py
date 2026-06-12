52ec1413a5a01e23910763f2ea8b89be2773f894
Katherine Varela <katherinevarela775@gmail.com>
2026-06-12 17:47:21 -0300

    feat: implementar algoritmo Minimax y funcion de evaluacion heuristica

--- minimax.py
+++ minimax.py
@@ -33,4 +33,39 @@ def generar_juego(tamano):
                 tablero[filas][colum] = "⬛" 
         
         if existe_camino(raton, salida, tamano, tablero): 
-            return {"tamano": tamano, "pos_gato": gato, "pos_raton": raton, "salida": salida, "tablero": tablero, "memoria": []} # Diccionario con
+            return {"tamano": tamano, "pos_gato": gato, "pos_raton": raton, "salida": salida, "tablero": tablero, "memoria": []} # Diccionario con la estructura del juego
+
+def puntuar(datos, es_raton): 
+    """Heurística: El motor de decisiones de la IA.""" # Heuristica: Dist. Manhattan para penalizacion
+    gato, raton, salida = datos["pos_gato"], datos["pos_raton"], datos["salida"] 
+    dist_gato_raton = abs(gato[0]-raton[0]) + abs(gato[1]-raton[1]) 
+    dist_raton_salida = abs(raton[0]-salida[0]) + abs(raton[1]-salida[1]) #Las dist. Manhattan las utilizo para la penalizacion
+
+    if es_raton:
+        penalizacion = datos["memoria"].count(raton) * 60 
+        return (dist_gato_raton * 5) - (dist_raton_salida * 60) - penalizacion + random.randint(0,5) 
+    return -(dist_gato_raton * 200) + random.randint(0,5) 
+
+def minimax(datos, prof, es_max): 
+    """Simulador de estados futuros."""
+    if datos["pos_gato"] == datos["pos_raton"]: return -20000 
+    elif datos["pos_raton"] == datos["salida"]: return 20000 
+    elif prof == 0: return puntuar(datos, es_max)  # Cuando la prof. llega a 0, deja de imaginar y utiliza la func. puntuar para evaluar que tan buena es la funcion actual
+
+    mejor_valor = float('-inf') if es_max else float('inf') 
+    yo = datos["pos_raton"] if es_max else datos["pos_gato"] 
+    otro = datos["pos_gato"] if es_max else datos["pos_raton"]
+    
+    for movimiento in movimientos_posibles(yo, otro, datos["tamano"], datos["tablero"], not es_max):
+        pos_original = tuple(yo) # ------------------> Conceptualizar mejor Backtracking
+
+        if es_max: datos["pos_raton"] = movimiento 
+        else: datos["pos_gato"] = movimiento 
+
+        valor_minimax = minimax(datos, prof - 1, not es_max) # Llamamos a la funcion minimax, le restamos 1 a la prof. y pregunatamos por el oponente (Recursividad)
+        mejor_valor = max(mejor_valor, valor_minimax) if es_max else min(mejor_valor, valor_minimax) # max y min comparan los dos valores que le entregues, max te devuelve el mayor y min el menor
+
+        if es_max: datos["pos_raton"] = pos_original 
+        else: datos["pos_gato"] = pos_original
+
+    return mejor_valor # Resultado de la comparacion de max y min
